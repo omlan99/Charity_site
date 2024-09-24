1e42054d78ff64859cabcb585b3ff8622a8935be
omlan99 <omlan10616@gmail.com>
2024-09-25 00:22:48 +0600

    third commit

--- js/script.js
+++ js/script.js
@@ -3,128 +3,38 @@ let donationBtn = getMyId("donationBtn");
 let historyBtn = getMyId("historyBtn");
 
 // adding an eventlisitener to donationBtn
-donationBtn.addEventListener('click', function(){
-    // bg-neutral-50
-    // bg-[#b4f461]
+donationBtn.addEventListener("click", function () {
+ 
+  // Adding background color and text color
+  donationBtn.classList.add("bg-[#b4f461]", "text-black");
 
-    donationBtn.classList.add('bg-[#b4f461]', 'text-black');
-    getMyId('donateCard').classList.remove("hidden");
-    historyBtn.classList.remove('bg-[#b4f461]','text-black');
-    historyBtn.classList.add('bg-neutral-50', 'text-gray-400');
-
-})
-
-historyBtn.addEventListener('click', function(){
-    historyBtn.classList.remove('bg-neutral-50', 'text-gray-400');
-    getMyId('donateCard').classList.add("hidden");
-    historyBtn.classList.add('bg-[#b4f461]', 'text-black');
-    donationBtn.classList.remove('bg-[#b4f461]');
-    donationBtn.classList.add('text-gray-400');
-})
-
-// getting the donate button
-let donate = getMyId("donate");
-
-// adding an evenllisitener to donate button
-donate.addEventListener('click', function(){
-    let mainBalance = getMyId("mainBalance").innerText;
-    let donatedBalance = getMyId("donatedBalance").innerText;
-
-// getting the input value after click  
-let donateAmount = parseFloat(getIdValue("donateAmount"));
-// checking the value criteria
-if(donateAmount > 0 && typeof donateAmount === 'number' ){
-    // converting the elements to number
-    let mainBalanceNum = parseFloat(mainBalance);
-    let donatedBalanceNum = parseFloat(donatedBalance);
-
-    // the calculation part
-    let newBalance = mainBalanceNum - donateAmount;
-    let totalDonated = donatedBalanceNum + donateAmount;
-
-    // returning the result to DOM
-    getMyId('mainBalance').innerText = newBalance;
-    getMyId('donatedBalance').innerText = totalDonated;
-
-    // to show modal
-    my_modal_2.showModal();
-}
-else{
-    alert('Invalid Input');
-}
-})
-
-// adding an evenllisitener to donate button
-
-let donate2 = getMyId("donate2");
+  // Removing the hidden class to display
+  getMyId("donateCard").classList.remove("hidden");
+  // Adding hidden class to history to hide
+  getMyId("history").classList.add("hidden");
+  // 
+  historyBtn.classList.remove("bg-[#b4f461]", "text-black");
+  historyBtn.classList.add("bg-neutral-50", "text-gray-400");
+});
+historyBtn.addEventListener("click", function () {
+  historyBtn.classList.remove("bg-neutral-50", "text-gray-400");
+  getMyId("donateCard").classList.add("hidden");
+  getMyId("history").classList.remove("hidden");
+  historyBtn.classList.add("bg-[#b4f461]", "text-black");
+  donationBtn.classList.remove("bg-[#b4f461]", "text-black");
+  donationBtn.classList.add("text-gray-400");
+});
 
 
-// adding an evenllisitener to donat button
-donate2.addEventListener('click', function(){
-    let mainBalance = getMyId("mainBalance").innerText;
+// Attach event listeners to the "Donate Now" buttons
+getMyId("donate").addEventListener("click", function () {
+  handleDonation("donateAmount", "donatedBalance", "heading1");
+});
 
-    // getting the input value after click  
-    let donateAmount2 = parseFloat(getIdValue("donateAmount2"));
-    let donatedBalance2 = getMyId("donatedBalance2").innerText;
-    // checking the value criteria
-    if(donateAmount2 > 0 && typeof donateAmount2 === 'number' ){
-        // converting the elements to number
-        let mainBalanceNum = parseFloat(mainBalance);
-        let donatedBalanceNum = parseFloat(donatedBalance2);
+getMyId("donate2").addEventListener("click", function () {
+  handleDonation("donateAmount2", "donatedBalance2", "heading2");
+});
 
-        // the calculation part
-        let newBalance = mainBalanceNum - donateAmount2;
-        let totalDonated = donatedBalanceNum + donateAmount2;
-
-        // returning the result to DOM
-        getMyId('mainBalance').innerText = newBalance;
-        getMyId('donatedBalance2').innerText = totalDonated;
-    
-        // to show modal
-        my_modal_2.showModal();
-    }
-    else{
-        alert('Invalid Input');
-    }
-    })
-
-// adding an evenllisitener to donate button
-
-let donate3 = getMyId("donate3");
-
-// adding an evenllisitener to donat button
-donate3.addEventListener('click', function(){
-    let mainBalance = getMyId("mainBalance").innerText;
-    let donatedBalance3 = getMyId("donatedBalance3").innerText;
-
-    // getting the input value after click  
-    let donateAmount3 = parseFloat(getIdValue("donateAmount3"));
-    // checking the value criteria
-    if(donateAmount3 > 0 && typeof donateAmount3 === 'number' ){
-        // converting the elements to number
-        let mainBalanceNum = parseFloat(mainBalance);
-        let donatedBalanceNum = parseFloat(donatedBalance3);
-
-        // the calculation part
-        let newBalance = mainBalanceNum - donateAmount3;
-        let totalDonated = donatedBalanceNum + donateAmount3;
-        // returning the result to DOM
-        getMyId('mainBalance').innerText = newBalance;
-        getMyId('donatedBalance3').innerText = totalDonated;
-    
-        // to show modal
-        my_modal_2.showModal();   
-    }
-    else{
-        alert('Invalid Input');
-    }
-    })
-
-
-
-    // history section creation
-     const history = document.createElement('div');
-     history.className ="bg-white"
-     history.innerHTML = `
-        
-     `
+getMyId("donate3").addEventListener("click", function () {
+  handleDonation("donateAmount3", "donatedBalance3", "heading3");
+});
--- js/utilities.js
+++ js/utilities.js
@@ -1,8 +1,60 @@
+// Common function to getelemnts ID
 function getMyId(id){
   return  document.getElementById(id);
 }
 
+// Common function to get input value 
 function getIdValue(id){
     return  document.getElementById(id).value;
   }
-  
+ 
+  // Common function to handle donation
+function handleDonation(inputId, donatedBalanceId, donationHeadingId) {
+  // Get the main balance element and its current value
+  const mainBalanceElement = getMyId("mainBalance");
+  let mainBalance = parseInt(mainBalanceElement.textContent);
+
+  // Get the donation input value for the specific section
+  let donationInput = getMyId(inputId); 
+  const donationAmount = parseInt(donationInput.value);
+
+  // Ensure input is a valid number and is less than or equal to main balance
+  if (
+    !isNaN(donationAmount) &&
+    donationAmount > 0 &&
+    donationAmount <= mainBalance
+  ) {
+    // Deduct from main balance
+    mainBalance -= donationAmount;
+    mainBalanceElement.textContent = mainBalance;
+
+    // Add to the respective section's donated balance
+    const donatedBalanceElement = getMyId(donatedBalanceId);
+    let currentDonatedBalance = parseInt(donatedBalanceElement.textContent);
+    donatedBalanceElement.textContent = currentDonatedBalance + donationAmount;
+
+    // show modal
+    my_modal_2.showModal();
+    // Get the heading text
+    let donateHeading = getMyId(donationHeadingId).textContent;
+
+    // Get current date and time
+    let currentTime = new Date().toLocaleString();
+
+    // History section creation
+    const historyData = document.createElement("div");
+    historyData.className = "bg-white p-4 rounded-lg border-grey-500 border-2 my-4";
+    historyData.innerHTML = `
+    <p class="font-medium text-xl">${donationAmount} taka is donated for ${donateHeading}</p>
+    <p>${currentTime}</p>
+ `;
+
+    // Add the history entry to the top of the history section
+    let historyBox = getMyId("history");
+    historyBox.insertBefore(historyData, historyBox.firstChild);
+
+    donationInput.value = "";
+  } else {
+    alert("Invalid donation amount or insufficient main balance!");
+  }
+}
